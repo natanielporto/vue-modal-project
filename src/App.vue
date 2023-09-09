e7e2ee8c427a375a12b5f81cf8b0e1d1b959fc1e
Nataniel Porto <natanielkporto@gmail.com>
2023-09-09 16:15:54 +0100

    all basic functionalities

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,73 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <p>Welcome</p>
+  <teleport to=".modals" v-if="showModal">
+    <!-- <Modal :header="header" :text="text" theme="sale" @close="toggleModal" /> -->
+    <Modal theme="" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#">sign up</a>
+        <a href="#">more info</a>
+      </template>
+      <h1>Sample title</h1>
+      <p>Sample text</p>
+    </Modal>
+  </teleport>
+
+  <teleport to=".modals" v-if="showModal2">
+    <Modal theme="sale" @close="toggleModal2">
+      <template v-slot:links>
+        <a href="#">sign up2</a>
+        <a href="#">more info2</a>
+      </template>
+      <h1>Sample title2</h1>
+      <p>Sample text2</p>
+    </Modal>
+  </teleport>
+  <button @click="toggleModal">Show modal</button>
+  <button @click="toggleModal2">Show modal2</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Modal from "./components/Modal.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Modal },
+  data() {
+    return {
+      title: "My first Vue app",
+      showModal: false,
+      showModal2: false,
+    };
+  },
+  methods: {
+    handleClick() {
+      this.$refs.name.classList.add("active");
+      this.$refs.name.focus();
+    },
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+    toggleModal2() {
+      this.showModal2 = !this.showModal2;
+    },
+  },
+};
 </script>
 
 <style>
-#app {
+#app,
+.modals {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
 }
 </style>
